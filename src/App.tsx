7aa71d826bf8deefebbab02253f840802cea3ccf
Victor Pedro <victorr.pgms@gmail.com>
2026-09-01 13:21:46 -0300

    ✨ New Feature: Implementação e atualização das rotas

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ function App() {
 
                 <Routes>
                     <Route path="/" element={<Home />} />
+                    <Route path="/home" element={<Home />} />
                 </Routes>
 
                 <Footer />
